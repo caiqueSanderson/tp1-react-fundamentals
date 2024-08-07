722a3f40102e1182faef0844ac8d2c3ebee0da8d
sanderson <cbsanderson.04@gmail.com>
2024-08-07 17:08:08 -0300

    exercicio 3

--- src/TP1/03/App.jsx
+++ src/TP1/03/App.jsx
@@ -1,5 +1,17 @@
+const avaliacoes = [1,2,3,4,5]
+function calcularMediaAvaliacoes(avaliacoes){
+    const soma = avaliacoes.reduce((a,b) => a + b, 0);
+    const media = soma / (avaliacoes.length + 1);
+    return media;
+  }
+
+  const resultado = calcularMediaAvaliacoes(avaliacoes);
+  console.log(resultado);
+  
 export default function App() {
     return (
-        <div>Hello World</div>
+        <div>
+            <h1>Média de Avaliações</h1>
+        </div>
     )
 }

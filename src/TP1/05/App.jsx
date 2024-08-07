2ace759f83a2cfb2248c178dfef86f139224e6b1
sanderson <cbsanderson.04@gmail.com>
2024-08-07 17:27:33 -0300

    exercicio 5 finalizado

--- src/TP1/05/App.jsx
+++ src/TP1/05/App.jsx
@@ -1,5 +1,22 @@
+function encontrarProdutoPorNome(produtos, nome) {
+    const pesquisa = nome.toLowerCase();
+    const produtoEncontrado = produtos.find(produto => produto.nome.toLowerCase() === pesquisa);
+    return produtoEncontrado;
+}
+
+const produtos = [
+    { nome: "Laptop", preco: 1000 },
+    { nome: "Tablet", preco: 400 },
+    { nome: "Smartphone", preco: 1500 }
+];
+
+const resultado = encontrarProdutoPorNome(produtos, "Laptop");
+console.log(resultado);
+
 export default function App() {
     return (
-        <div>Hello World</div>
-    )
+        <div>
+            <h1>Encontrar Produtos Por Nome</h1>
+        </div>
+    );
 }

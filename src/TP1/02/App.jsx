fc9a06cf33304ebaaa03ade4b9fca077bc67459f
sanderson <cbsanderson.04@gmail.com>
2024-08-07 17:01:31 -0300

    exercicio 2

--- src/TP1/02/App.jsx
+++ src/TP1/02/App.jsx
@@ -1,5 +1,23 @@
+const produtos = [ 
+    { nome: "Laptop", disponivel: true }, 
+    { nome: "Tablet", disponivel: false }, 
+    { nome: "Smartphone", disponivel: true } 
+];
+
+function filtrarDisponiveis(objeto){
+    const produtosDisponiveis = objeto.filter((item) => item['disponivel'] === true);
+    return produtosDisponiveis;
+}
+
+const produtosDisponiveis = filtrarDisponiveis(produtos);
+console.log(produtosDisponiveis);
+
 export default function App() {
+    
     return (
-        <div>Hello World</div>
+        <div>
+            <h2>Produtos Disponiveis</h2>
+            <p>Resposta no console</p>
+        </div>
     )
 }

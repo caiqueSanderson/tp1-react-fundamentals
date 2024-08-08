f70629a1dc52cf8b9040925040c573d8963f3fd3
sanderson <cbsanderson.04@gmail.com>
2024-08-08 12:02:54 -0300

    Finalizado exercício 6

--- src/TP1/06/App.jsx
+++ src/TP1/06/App.jsx
@@ -1,5 +1,24 @@
+function filtrarPorCategoria(produtos, nome){
+    const pesquisa = nome.toLowerCase();
+    const produtosFiltrados = produtos.filter(element => 
+        element.categoria.toLowerCase() === pesquisa
+    );
+    return produtosFiltrados;
+}
+
+const produtos = [ 
+    { nome: "Laptop", categoria: "Eletrônicos" }, 
+    { nome: "Camisa", categoria: "Roupas" }, 
+    { nome: "Smartphone", categoria: "Eletrônicos" } 
+    ];
+
+const resultado = filtrarPorCategoria(produtos, "Eletrônicos");
+console.log(resultado);
+
 export default function App() {
     return (
-        <div>Hello World</div>
+        <div>
+            <h1>Filtrar por Categoria</h1>
+        </div>
     )
 }

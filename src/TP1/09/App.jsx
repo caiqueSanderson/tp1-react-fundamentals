563a524254774cdeab06cc212df3ce27b735095a
sanderson <cbsanderson.04@gmail.com>
2024-08-08 14:33:30 -0300

    Finalizado exercício 9

--- src/TP1/09/App.jsx
+++ src/TP1/09/App.jsx
@@ -1,5 +1,31 @@
+function agruparPorCategoria(produtos){
+    const agrupadosPorCategoria = produtos.reduce((acumulador, produto) => {
+        const { categoria, nome } = produto;
+    
+        if (!acumulador[categoria]) {
+            acumulador[categoria] = [];
+        }
+    
+        acumulador[categoria].push(nome);
+        return acumulador;
+}, {});
+    return agrupadosPorCategoria;
+}
+
+const produtos = [ 
+    { nome: "Laptop", categoria: "Eletrônicos" }, 
+    { nome: "Camisa", categoria: "Roupas" }, 
+    { nome: "Smartphone", categoria: "Eletrônicos" }, 
+    { nome: "Calça", categoria: "Roupas" }, 
+    { nome: "Fone de Ouvido", categoria: "Eletrônicos"}, 
+];
+const agrupadosPorCategoria = agruparPorCategoria(produtos);
+console.log(agrupadosPorCategoria);
+
 export default function App() {
     return (
-        <div>Hello World</div>
+        <div>
+            <h1>Agrupar Por Categoria</h1>
+        </div>
     )
 }

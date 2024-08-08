eee31436a5d9ae7f328a23f2acbe5d7082694ac0
sanderson <cbsanderson.04@gmail.com>
2024-08-08 14:03:56 -0300

    Finalizado exercício 7

--- src/TP1/07/App.jsx
+++ src/TP1/07/App.jsx
@@ -1,5 +1,20 @@
+function verificaEstoque(produtos){
+    const produtosEmEstoque = produtos.filter(produto => produto.quantidadeEmEstoque > 0);
+    return produtosEmEstoque;
+}
+
+const produtos = [ 
+    { nome: "Laptop", quantidadeEmEstoque: 5 }, 
+    { nome: "Tablet", quantidadeEmEstoque: 0 }, 
+    { nome: "Smartphone", quantidadeEmEstoque: 3 } 
+];
+const produtosEmEstoque = verificaEstoque(produtos);
+console.log(produtosEmEstoque);
+
 export default function App() {
     return (
-        <div>Hello World</div>
+        <div>
+            <h1>Verifica Produtos Em Estoque</h1>
+        </div>
     )
 }
